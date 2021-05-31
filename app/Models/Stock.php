7f511af5fe9958140b6bad42a591917bad34b032
pankaj <pankaj@vm-jenkins-new.com>
2021-05-31 14:07:30 +0530

    change in feeds api

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -15,7 +15,19 @@ class Stock extends Model
 
     protected $hidden=['created_at', 'deleted_at', 'updated_at'];
 
+    protected $appends=['change'];
+
     public function watchlist(){
         return $this->belongsToMany('App\Models\Customer', 'stocks_watchlist', 'stock_id', 'customer_id');
+    }
+
+
+    public function getChangeAttribute(){
+
+        $change=round(rand(0,10000)/100, 2);
+        if(rand(0, 1))
+            return 0-$change;
+        return $change;
+
     }
 }

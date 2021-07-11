64acb34cb6e8a176392060bb78e12876657c5de8
pks11iitr <pks11iitr@gmail.com>
2021-07-11 21:36:44 +0530

    minor fix in following followers

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Notification extends Model
 {
@@ -10,9 +11,15 @@ class Notification extends Model
 
     protected $hidden = ['deleted_at','updated_at'];
 
-    protected $fillable=['user_id', 'title', 'description', 'type','type_id', 'var1','var2','var3', 'seen_at'];
+    protected $fillable=['user_id', 'title', 'description', 'type','type_id', 'var1','var2','var3', 'seen_at', 'image'];
 
     public function getCreatedAtAttribute($value){
         return date('m/d/Y h:ia', strtotime($value));
+    }
+
+    public function getImageAttribute($value){
+        if($value)
+            return Storage::url($value);
+        return Storage::url('customers/default.jpeg');
     }
 }
